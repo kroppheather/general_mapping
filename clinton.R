9fe240aa38a3300508fed413a7a7ff5e92ad47c7
kroppheather <hkropp@hamilton.edu>
2020-12-11 15:15:44 -0500

    looked at forest height

--- clinton.R
+++ clinton.R
@@ -2,8 +2,27 @@ library(sf)
 library(rgdal)
 library(raster)
 library(sp)
+library(OpenStreetMap)
 
-rds <- st_read("/Users/hkropp/Google Drive/GIS/streets/ny/map/roads-line.shp", package="sf")
+install.packages("OpenStreetMap")
 
-rds <- readOGR("/Users/hkropp/Google Drive/GIS/NY/streets/SimplifiedStreets_shp/SimplifiedStreetSegmentQrt.shp")
-plot(rds)
+
+rds <- readOGR("c:\\Users\\hkropp\\Google Drive\\GIS\\NY\\streets\\SimplifiedStreets_shp\\SimplifiedStreetSegmentQrt.shp")
+plot(rds, axes=TRUE)
+
+rds@
+
+rds@proj4string
+
+b <- as(extent(467469,471469,4763621,4768621),"SpatialPolygons")
+
+rdsC <- crop(rds,b)
+
+plot(rdsC,lwd=5)
+
+
+forest <- raster("c:\\Users\\hkropp\\Google Drive\\GIS\\forest\\Forest_height_2019_NAM.tif")
+forest@crs
+str(forest)
+
+plot(forest, ext=extent(-76,-74.5,42.5,43.5))
