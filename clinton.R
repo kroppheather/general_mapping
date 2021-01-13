a4e3384daff73b8a5c1f0ec5e7d97eac75d510bb
kroppheather <hkropp@hamilton.edu>
2021-01-13 15:56:51 -0500

    added some other plotting approaches

--- clinton.R
+++ clinton.R
@@ -26,3 +26,31 @@ forest@crs
 str(forest)
 
 plot(forest, ext=extent(-76,-74.5,42.5,43.5))
+
+writeOGR(rdsC,"c:\\Users\\hkropp\\Google Drive\\GIS\\NY\\streets\\clinton_street.shp","streets",driver="ESRI Shapefile")
+
+
+library(sf)
+library(ggplot2)
+library(cartography)
+library(smoothr)
+cR <- st_read("c:\\Users\\hkropp\\Google Drive\\GIS\\NY\\streets\\clinton_street.shp")
+cR2 <- st_simplify(cR)
+plot(cR)
+plot(cR)
+plot(cR2)
+
+ggplot()+
+  geom_sf(data=cR)+
+  theme_classic()
+str(cR)
+
+cT <- getTiles(cR, type=
+              "OpenStreetMap")
+tilesLayer(x=cT)
+plot(cR, add=TRUE)
+
+cRg <- smooth(cR, method="ksmooth")
+ggplot()+
+  geom_sf(data=cRg, size=0.5)+
+  theme_classic()
